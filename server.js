f872561773236dc9f46858c3adc9cb7e174318ec
igolkva <81855517+igolkva@users.noreply.github.com>
2024-01-13 16:40:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,3 +19,15 @@ app.get('/api/pages', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+// ... (previous code)
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/public/admin.html');
+});
+
+app.delete('/api/pages/:pageName', (req, res) => {
+    const pageName = req.params.pageName;
+    pages = pages.filter(page => page !== pageName);
+    res.sendStatus(204);
+});
